e2842e4509ebdca55aeca4a42d9208d360e9c3b4
NatalieFlin <90685429+NatalieFlin@users.noreply.github.com>
2021-10-11 11:11:35 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,16 @@
+<?php
+  $a = "blue";
+
+  if ($a == "black"){
+    echo "Черный";
+  }
+  elseif ($a == "white"){
+    echo "Белый";
+  }
+  elseif ($a == "blue"){
+    echo "голубой";
+    }
+  else{
+    echo ('Цвет не определен');
+  }
+ ?>
---
+++ one.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> PHP Program </title>
+</head>
+<body>
+<?php
+    $name = "Natalie";
+    $age = 16;
+
+    echo "Меня зовут $name <br>";
+    print "Мне $age лет"
+?>
+</body>
+</html>
